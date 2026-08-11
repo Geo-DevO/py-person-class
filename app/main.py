38135cf48e536b5b739b438d2b7c8d44b82af6f4
Heorhii Babchenko <zor1kk88@gmail.com>
2026-08-11 21:29:34 +0200

    Fix: replace for loop with list comprehension and remove pass

--- app/main.py
+++ app/main.py
@@ -8,11 +8,10 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-
-    for person_dict in people:
-        person = Person(person_dict["name"], person_dict["age"])
-        person_list.append(person)
+    person_list = [
+        Person(person_dict["name"], person_dict["age"])
+        for person_dict in people
+    ]
 
     for person_dict, person in zip(people, person_list):
         for key in ("wife", "husband"):
